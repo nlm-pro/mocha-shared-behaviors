d43735fb91e2e4a8b9262c092b388c07c4c00eda
Nwel <nweldev@users.noreply.github.com>
2019-08-21 23:02:27 +0200

    refactor: better api for all-in-one

--- test/all-in-one/helpers.js
+++ test/all-in-one/helpers.js
@@ -1,27 +1,23 @@
 import { expect } from '@bundled-es-modules/chai';
 
-export function shouldBehaveLikeAUser(constructor) {
+export function shouldBehaveLikeAUser(generateUser, { firstName, lastName, fullname }) {
   let userLike;
-  const firstName = 'tobi';
-  const lastName = 'holowachuk';
 
-  context(`when using new ${constructor.name}(${firstName}, ${lastName})`, () => {
-    beforeEach(() => {
-      userLike = new constructor(firstName, lastName);
+  beforeEach(() => {
+    userLike = generateUser();
+  });
+  
+  it('should have .name.first', () => {
+    expect(userLike.name.first).to.equal(firstName);
+  });
+  
+  it('should have .name.last', () => {
+    expect(userLike.name.last).to.equal(lastName);
+  });
+  
+  describe('.fullname()', () => {
+    it('should return the full name', () => {
+      expect(userLike.fullname()).to.equal(fullname);
     });
-    
-    it('should have .name.first', () => {
-      expect(userLike.name.first).to.equal(firstName);
-    });
-    
-    it('should have .name.last', () => {
-      expect(userLike.name.last).to.equal(lastName);
-    });
-    
-    describe('.fullname()', () => {
-      it('should return the full name', () => {
-        expect(userLike.fullname()).to.equal(`${firstName} ${lastName}`);
-      });
-    });
-  })
+  });
 }
--- test/all-in-one/user.test.js
+++ test/all-in-one/user.test.js
@@ -2,9 +2,11 @@ import { User } from '../../src/user.js';
 import { shouldBehaveLikeAUser } from './helpers.js';
 
 context('Solution: all-in-one', () => {
-
   describe('User', () => {
-    shouldBehaveLikeAUser(User);
+    shouldBehaveLikeAUser(() => new User('tobi', 'holowaychuk'), {
+      firstName: 'tobi',
+      lastName: 'holowaychuk',
+      fullname: 'tobi holowaychuk'
+    });
   });
-  
 });
