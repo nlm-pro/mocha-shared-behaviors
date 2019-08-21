d6033c85c7fe9f3747b5fd93273cdcab2d5da747
NM <nlm-pro@users.noreply.github.com>
2019-08-21 23:02:27 +0200

    refactor: better api for all-in-one

--- test/all-in-one/admin.test.js
+++ test/all-in-one/admin.test.js
@@ -4,7 +4,11 @@ import { expect } from '@bundled-es-modules/chai';
 
 context('Solution: all-in-one', () => {
   describe('Admin', () => {
-    shouldBehaveLikeAUser(Admin);
+    shouldBehaveLikeAUser(() => new Admin('tobi', 'holowaychuk'), {
+      firstName: 'tobi',
+      lastName: 'holowaychuk',
+      fullname: 'tobi holowaychuk'
+    });
 
     it('should be an .admin', () => {
       expect(new Admin().admin).to.be.true;
